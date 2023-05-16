e1bfebbe4bdda13e580c68118a6996fc3bce5e77
TheDiscoMole <kim.roder@gmail.com>
2023-05-16 13:49:31 +0200

    adding convnd support

--- LoRA.py
+++ LoRA.py
@@ -5,6 +5,44 @@ class Layer ():
         # modules
         self.tasks = {}
 
+class ConvNdTask (torch.nn.Module):
+    def __init__ (self, in_channels, out_channels,
+        kernel_size=5,
+        rank=16,
+        **kwargs
+    ):
+        torch.nn.Module.__init__(self)
+
+        # validation
+        assert (rank > 0) and (rank < min(in_channels, out_channels)), "rank must be between 0 and smallest dimension"
+
+        # configs
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.rank = rank
+
+        # parameters
+        self.A = torch.nn.parameters.Parameter(torch.zeros(in_channels * kernel_size, rank * kernel_size))
+        self.B = torch.nn.parameters.Parameter(torch.zeros(rank * kernel_size, out_channels * kernel_size))
+
+        # modules
+        match dims:
+            case 1: self.convolution = torch.nn.functional.conv1d
+            case 2: self.convolution = torch.nn.functional.conv2d
+            case 3: self.convolution = torch.nn.functional.conv3d
+
+    def forward (self, input):
+        # convolution
+        kernel = torch.matmul(self.A, self.B)
+        convolution = self.convolution(input, kernel)
+
+        return convolution
+
+    def reset_parameters (self):
+        self.A = torch.nn.parameters.Parameter(torch.zeros(self.in_channels * self.kernel_size, self.rank * self.kernel_size))
+        self.B = torch.nn.parameters.Parameter(torch.zeros(self.rank * self.kernel_size, self.out_channels * self.kernel_size))
+
 class EmbeddingTask (torch.nn.Module):
     def __init__ (self, length, features,
         rank=16
@@ -12,7 +50,12 @@ class EmbeddingTask (torch.nn.Module):
         torch.nn.Module.__init__(self)
 
         # validation
-        assert (rank > 0) and (rank < min(in_features, out_features)), "rank must be between 0 and lowest number of features"
+        assert (rank > 0) and (rank < min(length, features)), "rank must be between 0 and smallest dimension"
+
+        # configs
+        self.length = length
+        self.features = features
+        self.rank = rank
 
         # parameters
         self.A = torch.nn.parameters.Parameter(torch.zeros(length, rank))
@@ -26,8 +69,8 @@ class EmbeddingTask (torch.nn.Module):
         return embedding
 
     def reset_parameters (self):
-        self.A = torch.nn.parameters.Parameter(torch.zeros(length, rank))
-        self.B = torch.nn.parameters.Parameter(torch.zeros(rank, features))
+        self.A = torch.nn.parameters.Parameter(torch.zeros(self.length, self.rank))
+        self.B = torch.nn.parameters.Parameter(torch.zeros(self.rank, self.features))
 
 class LinearTask (torch.nn.Module):
     def __init__ (self, in_features, out_features,
@@ -36,7 +79,12 @@ class LinearTask (torch.nn.Module):
         torch.nn.Module.__init__(self)
 
         # validation
-        assert (rank > 0) and (rank < min(in_features, out_features)), "rank must be between 0 and lowest number of features"
+        assert (rank > 0) and (rank < min(in_features, out_features)), "rank must be between 0 and smallest dimension"
+
+        # configs
+        self.in_features = in_features
+        self.out_features = out_features
+        self.rank = rank
 
         # parameters
         self.A = torch.nn.parameters.Parameter(torch.zeros(in_features, rank))
@@ -50,8 +98,8 @@ class LinearTask (torch.nn.Module):
         return linear
 
     def reset_parameters (self):
-        self.A = torch.nn.parameters.Parameter(torch.zeros(in_features, rank))
-        self.B = torch.nn.parameters.Parameter(torch.zeros(rank, out_features))
+        self.A = torch.nn.parameters.Parameter(torch.zeros(self.in_features, self.rank))
+        self.B = torch.nn.parameters.Parameter(torch.zeros(self.rank, self.out_features))
 
 def reset_parameters (model, task):
     for module in model.modules():
