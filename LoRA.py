3b4ccbef9c25aa7dba862296d1289a5ae6c1a6b4
TheDiscoMole <kim.roder@gmail.com>
2023-05-17 18:28:44 +0200

    cleaning up and fixing remaining functionality

--- LoRA.py
+++ LoRA.py
@@ -1,3 +1,4 @@
+import collections
 import math
 import torch
 
@@ -36,6 +37,8 @@ class ConvNd (torch.nn.Module):
         self.A = torch.nn.Parameter(torch.zeros(*self.kernel_size, self.out_channels, rank))
         self.B = torch.nn.Parameter(torch.zeros(*self.kernel_size, rank, self.in_channels))
 
+        self.kernel = (self.A @ self.B).view(out_channels, in_channels, *self.kernel_size)
+
         # modules
         match dims:
             case 1: self.convolution = torch.nn.functional.conv1d
@@ -43,11 +46,7 @@ class ConvNd (torch.nn.Module):
             case 3: self.convolution = torch.nn.functional.conv3d
 
     def forward (self, input):
-        # convolution
-        kernel = torch.matmul(self.A, self.B).view(self.out_channels, self.in_channels, *self.kernel_size)
-        convolution = self.convolution(input, kernel, padding=self.padding, stride=self.stride)
-
-        return convolution
+        return self.convolution(input, self.kernel, padding=self.padding, stride=self.stride)
 
 class Embedding (torch.nn.Module):
     """
@@ -74,12 +73,10 @@ class Embedding (torch.nn.Module):
         self.A = torch.nn.Parameter(torch.zeros(num_embeddings, rank))
         self.B = torch.nn.Parameter(torch.zeros(rank, embedding_dim))
 
-    def forward (self, idx):
-        # embedding
-        weight = torch.matmul(self.A, self.B)
-        embedding = torch.nn.functional.embedding(idx, weight)
+        self.weight = self.A @ self.B
 
-        return embedding
+    def forward (self, idx):
+        return torch.nn.functional.embedding(idx, self.weight)
 
 class Linear (torch.nn.Module):
     """
@@ -106,12 +103,10 @@ class Linear (torch.nn.Module):
         self.A = torch.nn.Parameter(torch.zeros(in_features, rank))
         self.B = torch.nn.Parameter(torch.zeros(rank, out_features))
 
-    def forward (self, input):
-        # linear
-        weight = torch.matmul(self.A, self.B)
-        linear = torch.nn.functional.linear(input, weight)
+        self.weight = self.A @ self.B
 
-        return linear
+    def forward (self, input):
+        return torch.nn.functional.linear(input, self.weight)
 
 class LoRALayer (torch.nn.Module):
     """
@@ -146,10 +141,24 @@ class LoRALayer (torch.nn.Module):
         del self.tasks[task]
 
     def parameters (self, task=None):
-        yield self.module.parameters() if task is None else self.tasks[task].parameters()
+        return self.module.parameters() if task is None else self.tasks[task].parameters()
 
     def train (self, task=None, mode=True):
         self.module.train(mode=mode) if task is None else self.tasks[task].train(mode=mode)
+
+    def state_dict (self, task=None, prefix="", **kwargs):
+        if task is None:
+            return self.module.state_dict(prefix=f"{prefix}module.", **kwargs)
+        else:
+            return self.tasks[task].state_dict(prefix=f"{prefix}module.", **kwargs)
+
+    def load_state_dict (self, state_dict, task=None, prefix="", **kwargs):
+        if task is None:
+            self.module.load_state_dict(state_dict, **kwargs)
+        else:
+            prefix = f"{prefix}module."
+            state_dict = {k[len(prefix)]:v for k,v in state_dict.items() if prefix in k}
+            self.tasks[task].load_state_dict(state_dict, **kwargs)
 
 class LoRAModel (torch.nn.Module):
     """
@@ -167,20 +176,16 @@ class LoRAModel (torch.nn.Module):
         # compile
         def compile (model):
             for name, module in model.named_children():
-                compile(module)
-
                 if isinstance(module, convolution.ConvNd):
                     new_task = lambda module: ConvNd(dims=module.dims, in_channels=module.in_channels, out_channels=module.out_channels, kernel_size=module.kernel_size, padding=module.padding, rank=rank(module.in_channels, module.out_channels), stride=module.stride)
-                    module = LoRALayer(self, module, new_task)
+                    setattr(model, name, LoRALayer(self, module, new_task))
                 elif isinstance(module, torch.nn.Embedding):
                     new_task = lambda module: Embedding(num_embeddings=module.num_embeddings, embedding_dim=module.embedding_dim, rank=rank(module.num_embeddings, module.embedding_dim))
-                    module = LoRALayer(self, module, new_task)
+                    setattr(model, name, LoRALayer(self, module, new_task))
                 elif isinstance(module, torch.nn.Linear):
                     new_task = lambda module: Linear(in_features=module.in_features, out_features=module.out_features, rank=rank(module.in_features, module.out_features))
-                    module = LoRALayer(self, module, new_task)
-                else: continue
-
-                setattr(model, name, module)
+                    setattr(model, name, LoRALayer(self, module, new_task))
+                else: compile(module)
         compile(model)
 
         # model
@@ -194,39 +199,57 @@ class LoRAModel (torch.nn.Module):
 
         return output
 
-    def add_task (self, task):
+    def lora_layers (self):
         for module in self.modules():
-            if isinstance(module, LoRALayer): module.add_task(task)
+            if isinstance(module, LoRALayer): yield module
+
+    def add_task (self, task):
+        for module in self.lora_layers(): module.add_task(task)
 
     def remove_task (self, task):
-        for module in self.modules():
-            if isinstance(module, LoRALayer): module.remove_task(task)
+        for module in self.lora_layers(): module.remove_task(task)
 
     def parameters (self, task=None):
-        for module in self.modules():
-            if isinstance(module, LoRALayer): yield module.parameters(task=task)
-            elif task is None: yield module.parameters()
+        if task is None:
+            for param in self.model.parameters(): yield param
+            return
+
+        for module in self.lora_layers():
+            for param in module.parameters(task=task): yield param
+
+    def eval (self, task=None):
+        self.train(task=task, mode=False)
 
     def train (self, task=None, mode=True):
-        for module in self.modules():
-            if isinstance(module, LoRALayer): module.train(task=task, mode=mode)
-            elif task is None: module.train(mode=mode)
+        self.training = mode
 
-    def state_dict (model, task=None, prefix=""):
-        dict = {}
+        if task is None: return self.model.train(mode=mode)
+        for module in self.lora_layers(): module.train(task=task, mode=mode)
 
-        def df (module, task=None, prefix=""):
+    def state_dict (self, task=None, prefix="", **kwargs):
+        if task is None: return self.model.state_dict(destination=collections.OrderedDict(), prefix=prefix)
+
+        def df_task_state_dict (module, task, prefix):
+            task_state_dict = {}
+
+            if isinstance(module, LoRALayer):
+                return module.state_dict(task=task, prefix=f"{prefix}{task}.")
+
             for name,child in module.named_children():
-                dict = {**dict, **df(child, task, prefix=f"{prefix}.{name}")}
+                task_state_dict = {**task_state_dict, **df_task_state_dict(child, task, prefix=f"{prefix}{name}.")}
 
-        if isinstance(model, LoRALayer):
-            dict = {**dict, **model.tasks[task].state_dict(prefix=f"{prefix}.{task}")}
+            return task_state_dict
 
-        return dict
+        return collections.OrderedDict(df_task_state_dict(self.model, task, prefix))
 
-    def load_state_dict (model, task, state_dict, prefix=""):
-        for name,child in model.named_children():
-            load_state_dict(child, task, prefix=f"{prefix}.{name}")
+    def load_state_dict (self, state_dict, task=None, prefix="", **kwargs):
+        if task is None: return self.model.load_state_dict(state_dict)
 
-        if isinstance(model, LoRALayer):
-            module.tasks[task]._load_from_state_dict(state_dict=state_dict, prefix=f"{prefix}.{task}", local_metadata={}, strict=False, missing_keys=[], unexpected_keys=[], error_msgs=[])
+        def df_load_task_state_dict (module, state_dict, task, prefix):
+            if isinstance(module, LoRALayer):
+                return module.load_state_dict(state_dict, task=task, prefix=f"{prefix}{task}.")
+
+            for name,child in module.named_children():
+                df_load_task_state_dict (child, state_dict, task, prefix=f"{prefix}{name}.")
+
+        df_load_task_state_dict(self.model, state_dict, task, prefix)
