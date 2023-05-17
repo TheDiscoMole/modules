7cde582c5984f22eb46fa3b9ab573ade53969bcd
TheDiscoMole <kim.roder@gmail.com>
2023-05-17 11:07:05 +0200

    simplifying text

--- text.py
+++ text.py
@@ -2,47 +2,18 @@ import torch
 
 from .attention import *
 
-class SubWordToSentenceEncoder (torch.nn.Module):
-    def __init__ (self, embedding,
-        attention_heads=8,
-        dropout=1e-1,
-        embeddings_file=None,
-        layers=1
-    ):
-        super(TextEncoder, self).__init__()
-
-        # validation
-        assert len(embeddings.shape) == 2, "embeddings tensor must have 2 dimensions"
-        assert all(size == expected for size,expected in zip(embeddings.shape,shape)), "embeddings shape doesn't match definition"
-
-        # configs
-        self.features = shape[-1]
+class TextEncoder (torch.nn.Module):
+    def __init__ (self, embedder, encoder):
+        torch.nn.Module.__init__(self)
 
         # modules
-        self.embeddings = torch.nn.Embedding.from_pretrained(embeddings)
-        self.encoders = torch.nn.ModuleList([
-            AttentionEncoder(
-                MultiHeadAttention(self.features, attention_heads, dropout=dropout),
-                shape[-1],
-                AttentionInputNd(self.features, self.features, dropout=dropout),
-                AttentionInputNd(self.features, self.features, dropout=dropout),
-                AttentionInputNd(self.features, self.features, dropout=dropout),
-                dropout=dropout)
-            for i in range(layers)])
-
+        self.embedder = embedder
+        self.encoder = encoder
 
     def forward (self, text):
-        # validation
-        assert ids.dims() == 2, "input must have 2 dimensions"
-
-        # embedding
-        encoding = self.embeddings(ids)
-
         # encoding
-        for i,encoder in enumerate(self.encoders):
-            encoding = self.encoder(encoding, encoding, encoding)
-
-        # sentence
-        encoding = torch.sum(encoding, dim=1)
+        encoding = self.embedder(text)
+        encoding = self.encoder(encoding, encoding, encoding)
+        encoding = torch.mean(encoding, dim=1)
 
         return encoding
