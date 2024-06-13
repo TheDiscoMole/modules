700b22d2a555185284f71d7007756aa8c9a4643d
kim <kim.roder@gmail.com>
2024-06-13 12:55:07 +0200

    grabbign all imports

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .attend import MultiHeadAttention, AttentionEncoder
 from .convolve import ConvNd, ConvNdFeatureEncoder
+from .decussate import Decussation
 from .normalize import BatchNormNd
-from .scale import ConvTransposeNd, MaxPoolNd
+from .scale import ConvTransposeNd, MaxPoolNd, UpscaleNdFeatures
 from .shape import ReshapeSimple, ReshapeNdFlatten, ReshapePatchesNdBatches, ReshapePatchesNdFeatures
